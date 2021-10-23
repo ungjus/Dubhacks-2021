faf4e6cf01a6e78b8c857d44d548649c7d5cf2ac
ungjus <justinyong89@gmail.com>
2021-10-23 12:03:47 -0700

    pushed components folder

---
+++ website/husky-q-up/src/components/MainPage/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main = () => {
+    return(<main>
+        <h1>hello world!</h1>
+    </main>)
+}
+
+export default Main
--- website/husky-q-up/src/index.js
+++ website/husky-q-up/src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Main from './components/MainPage/main'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Main />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const IndexPage = () => {
+//   return (
+//     <main>
+//       <Main/>
+//     </main>
+//   )
+// }
+// export default IndexPage
+
