03419f59e7a116d2298b1a26aa1a2baeba9b70b4
ungjus <justinyong89@gmail.com>
2021-10-24 04:10:25 -0700

    plotly changes

--- website/husky-q-up/src/components/Dropdown/dropdown.js
+++ website/husky-q-up/src/components/Dropdown/dropdown.js
@@ -27,10 +27,36 @@ const Dropdown = ({ id, locations, getLocation, getTableData }) => {
         getTableData(loc.value);
         console.log(loc.value);
     }
+    const customStyles = {
+        option: (provided) => ({
+            ...provided,
+            padding: 20,
+        }),
+ 
+        singleValue: (provided, state) => {
+            const opacity = state.isDisabled ? .5 : .8;
+            const transition = 'opacity 300ms';
+        
+            return { ...provided, opacity, transition };
+        }
+      }
+    // const customStyles = {
+    //     control: styles => ({ ...styles, backgroundColor: 'white' }),
+    //     option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+    //       const color = chroma(data.color);
+          
+    //         ...styles,
+    //         backgroundColor: isDisabled ? 'red' : blue,
+    //         color: '#FFF',
+    //         cursor: isDisabled ? 'not-allowed' : 'default',
+    //         ...
+    //     }
+    //   }
 
     return (
         <div>
             <Select
+                styles={customStyles}
                 options={generateOptions()}
                 onChange={id === "user" ? handleChangeUser : handleChangeAdmin}
             />
