db1fea7b47479e12e7207d7f1457c4b7708c4663
ungjus <justinyong89@gmail.com>
2021-10-24 04:49:27 -0700

    added logo to footer

--- website/husky-q-up/src/components/Footer/footer.js
+++ website/husky-q-up/src/components/Footer/footer.js
@@ -1,8 +1,15 @@
 import React from "react";
-
+import logo from "../Footer/logo.png"
+import { Link } from "react-router-dom"
 import './footer.css'
 const Footer = () => (
   <footer>
+    <Link to ="/">
+      <img id="logo"
+          src={logo}
+          alt="Husky Q Up logo"
+      />
+    </Link>
     <p>&copy; Husky Q Up</p>
   </footer>
 );
