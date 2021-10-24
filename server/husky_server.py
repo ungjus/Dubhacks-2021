10a450cd85a74f3cb0ccc3e9446080a5b09f3249
ctuguina <ctuguina@uw.edu>
2021-10-23 17:39:07 -0700

    Update husky_server.py

--- server/husky_server.py
+++ server/husky_server.py
@@ -2,7 +2,7 @@ import json
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
 from postgre import connect, create_tables, table_exists, add_historical_data, add_one_person_to_line
-from postgre import remove_first_person, check_number_in_line, drop_table
+from postgre import remove_first_person, check_number_in_line, drop_table, remove_specific_person
 
 conn = connect()
 
@@ -32,19 +32,29 @@ def add_person(profile):
     print("Your number is:")
     print(number_in_line)
     print("Number of people in line:")
-    print("Predicted amount of time to wait in line:")
+    print(check_number_of_people_in_line(conn, table_name))
+    #print("Predicted amount of time to wait in line:")
+
 
 @sio.on("Remove Person")
 def remove_person(profile):
     print('removed person!')
     print(profile)
+    email = profile['email']
+    location = profile['location']
+    if location == "pagliaccis":
+        table_name = "pagliacci_current"
+    else:
+        table_name = "lander_desk_current"
+    remove_specific_person(conn, table_name, email)
+    print("Number of people in line:")
+    print(check_number_of_people_in_line(conn, table_name))
+
 
 @sio.on("Send Location")
 def send_location():
     print('sending locations')
     sio.emit('Get Locations', locations)
-
-
 
 
 if __name__ == '__main__':
